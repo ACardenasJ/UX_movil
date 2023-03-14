8687f449b98a6aa77e64ba0944808ef0ff28cced
Andres Cardenas <a.cardenasj@uniandes.edu.co>
2023-03-14 00:01:57 -0500

    4 pantalla

--- Ux_movil/app/src/main/java/com/example/uxmovil/FourthFragment.kt
+++ Ux_movil/app/src/main/java/com/example/uxmovil/FourthFragment.kt
@@ -5,14 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.uxmovil.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
 
+class FourthFragment : Fragment() {
     private var _binding: FragmentSecondBinding? = null
 
     // This property is only valid between onCreateView and
@@ -33,11 +29,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment)
-        }
-
-        binding.buttonVoz.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FourthFragment)
+            // findNavController().navigate(R.id.action_se)
         }
 
     }
